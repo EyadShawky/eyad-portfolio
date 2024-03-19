94de7ed20d38778f0ef3586c0fb66f0bade8bfe5
EyadShawky <eyadshawky0@gmail.com>
2024-03-19 04:20:17 +0200

    added the speed insghit of verical app

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Header from './Header'
-import { Outlet } from 'react-router-dom'
+// LayoutMain.jsx
+import React from 'react';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/next'; // Add the import here
 
 const LayoutMain = () => {
     return (
         <>  
-        <Header />
-        <div className=' mt-2'> 
-            <Outlet />
-        </div>
+            <Header />
+            <SpeedInsights /> 
+            <div className='mt-2'> 
+                <Outlet />
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default LayoutMain
+export default LayoutMain;
