88f826bef12843aed5efce514c8a65acb0d7f50d
EyadShawky <eyadshawky0@gmail.com>
2024-03-19 05:50:36 +0200

    fixed the error

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/next'; // Add the import here
 
 const LayoutMain = () => {
     return (
         <>  
             <Header />
-            <SpeedInsights /> 
             <div className='mt-2'> 
                 <Outlet />
             </div>
