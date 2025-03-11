4cadddb3010fe023d4918fad2d37c1a391655b85
Eyad Shawky <73734657+EyadShawky@users.noreply.github.com>
2025-03-11 19:38:06 +0200

    update portfolio

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,16 +11,22 @@ import tatweerBackground from '../images/tatweer-background.png';
 import marinaBackground from '../images/marina-background.png';
 import secureCipherBackground from '../images/secureCipherBackground.png';
 import ecommerceBackground from '../images/ecommerce-background.png';
+import DIGITISED from '../images/DIGITISED.png';
+import aallie from '../images/aallie.png';
 
 const Projects = () => {
     // Array of project objects with titles and background images
     const projects = [
+        { title: 'DIGITISED', background: DIGITISED, link: 'https://digitised.net/' },
+        { title: 'Aallie', background: aallie, link: 'https://aallie.com/Home' },
+        { title: 'Clipify', background: ClipfyBackground, link: 'https://www.behance.net/gallery/194226521/clipify' },
         { title: 'Progate', background: progateBackground, link: 'https://www.behance.net/gallery/194225825/E-Learning-V2-(Progate)' },
         { title: 'Qalam', background: qalamBackground, link: 'https://www.behance.net/gallery/194225693/E-Learning-V1-(Qalam)' },
         { title: 'Queuing System for Tatweer misr', background: tatweerBackground, link: 'https://www.behance.net/gallery/194225901/Queuing-System' },
         { title: 'Marina', background: marinaBackground, link: 'https://www.behance.net/gallery/194225997/Marina' },
         { title: 'Secure Cipher', background: secureCipherBackground, link: 'https://computer-security.vercel.app/' },
-        { title: 'E-Commerce', background: ecommerceBackground, link: 'https://www.behance.net/gallery/194226217/E-Commerce' }
+        { title: 'E-Commerce', background: ecommerceBackground, link: 'https://www.behance.net/gallery/194226217/E-Commerce' },
+       
     ];
 
     return (
@@ -35,12 +41,7 @@ const Projects = () => {
                     </div> 
  
                     <div className='row mt-4'>
-                        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 card-project mb-4" style={{ backgroundImage: `url(${ClipfyBackground})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', width: '98.5%', height: '100%' }}>
-                            <Link to="https://www.behance.net/gallery/194226521/clipify" className='d-flex justify-content-start align-items-end ' style={{ height: '100%' }}>
-                                    <h4 className='text-white'>Clipify</h4>
-                            </Link>
-                        </div>
-                        {/* Mapping over the projects array */}
+                       
                         {projects.map((project, index) => (
                             <Link key={index} to={project.link} className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 card-project mb-4" style={{ backgroundImage: `url(${project.background})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '100%', textDecoration: 'none' }}>
                                 <div className='d-flex justify-content-start align-items-end' style={{ height: '100%' }}>
